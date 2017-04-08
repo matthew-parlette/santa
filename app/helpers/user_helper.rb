03f7307d78782a840063ab5b3503441a764ccf7f
Matt Parlette <matthew.parlette@gmail.com>
2017-04-08 19:26:14 -0400

    Added first and last name for users

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,9 @@
 module UserHelper
+  def name(user)
+    if User.count(first_name: user.first_name) > 1
+      user.first_name + user.last_name
+    else
+      user.first_name
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'matthew.parlette@gmail.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'user1@santa.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'user2@santa.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'user3@santa.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'user4@santa.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'user5@santa.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'user6@santa.com', password: 'password', password_confirmation: 'password')
+User.create!(first_name: 'user1', last_name: 'lastname1', email: 'user1@santa.com', password: 'password', password_confirmation: 'password')
+User.create!(first_name: 'user2', last_name: 'lastname2', email: 'user2@santa.com', password: 'password', password_confirmation: 'password')
+User.create!(first_name: 'user3', last_name: 'lastname3', email: 'user3@santa.com', password: 'password', password_confirmation: 'password')
+User.create!(first_name: 'user4', last_name: 'lastname4', email: 'user4@santa.com', password: 'password', password_confirmation: 'password')
+User.create!(first_name: 'user5', last_name: 'lastname5', email: 'user5@santa.com', password: 'password', password_confirmation: 'password')
+User.create!(first_name: 'user6', last_name: 'lastname6', email: 'user6@santa.com', password: 'password', password_confirmation: 'password')
 
 User.all.each do |user|
   user.assignment_bans.create!(:assigned_to => user)
