87b86c71b47a20134f555bc4e86abeeea55e527c
Matt Parlette <matthew.parlette@gmail.com>
2017-06-30 08:57:41 -0400

    Fixed admin user show page
    Fixes #15

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,2 +1,12 @@
 module AssignmentsHelper
+  def color(user)
+    color = ''.ljust(2, user.id.to_s)
+    color += ''.ljust(2, Digest::MD5.hexdigest(user.first_name.to_s))
+    color += ''.ljust(2, Digest::MD5.hexdigest(user.email.to_s))
+    return color
+  end
+
+  def hidden_name(user)
+    return Digest::MD5.hexdigest(user.id.to_s)
+  end
 end
